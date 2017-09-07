d6f72f29bd3b709f4fe8bbf0d6dcbfd57ee3b535
Afonasev Evgeniy <ea.afonasev@gmail.com>
2017-09-07 18:39:20 +0500

    next

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.urls import reverse
 from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 from backend.utils import markdown_to_html
 
